1c41170be85743241f3eb7da15649da013201525
Nikki Alistra <nikkialistra@gmail.com>
2021-05-18 16:03:17 +0500

    Change view to first person

---
+++ Assets/Scripts/Welder/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Welder
+{
+    public class MouseLook : MonoBehaviour
+    {
+        [SerializeField] private Transform _welder;
+        
+        [SerializeField] private float mouseSensitivity = 100f;
+
+        private float yRotation;
+        
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            yRotation -= mouseY;
+            yRotation = Mathf.Clamp(yRotation, -90f, 90);
+            
+            transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+            _welder.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
---
+++ Assets/Scripts/Welder/Movement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Welder
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 12f;
+        [SerializeField] private float _gravity = 15;
+
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private float _groundDistance = 0.3f;
+        [SerializeField] private LayerMask _groundMask;
+
+        private Vector3 _velocity;
+        private bool _isGrounded;
+
+        private CharacterController _controller;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+        }
+
+        private void Update()
+        {
+            UpdateFalling();
+            UpdateMoving();
+        }
+
+        private void UpdateMoving()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+
+            var move = transform.right * x + transform.forward * z;
+
+            _controller.Move(move * (_speed * Time.deltaTime));
+        }
+
+        private void UpdateFalling()
+        {
+            _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+            if (_isGrounded)
+            {
+                _velocity.y = -2f;
+            }
+            else
+            {
+                _velocity.y += _gravity * Time.deltaTime;
+            }
+
+            _controller.Move(_velocity * Time.deltaTime);
+        }
+    }
+}
